655f027557a0081c85a6c9f072ee0ae757a760d2
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-01-22 15:31:22 +0300

    Inc

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
+    const name = this.querySelector('input[type="text"]').value;
     const email = this.querySelector('input[type="email"]').value;
     
-    // Here you would typically send the email to your backend
-    console.log('Schedule requested for:', email);
+    // Here you would typically send the data to your backend
+    console.log('Schedule requested for:', name, email);
     alert('Thank you! We will contact you shortly.');
 });
