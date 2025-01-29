1d11fffcde698fbb8cb5dacaeeafbdb761afa139
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-01-29 17:55:58 +0300

    Add social links

--- assets/script.js
+++ assets/script.js
@@ -8,52 +8,9 @@ function renderBodyJson(name, company, email) {
     };
 }
 
-document.getElementById('contact-form').addEventListener('submit', function (e) {
+document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    const name = this.querySelector('[name="name"]').value;
-    const company = this.querySelector('[name="company"]').value;
-    const email = this.querySelector('[name="email"]').value;
-
-    const bodyJson = {
-        "surveyId": "6797038e84227c1a123e374c",
-        "values": {
-            "name": name,
-            "company": company,
-            "email": email
-        },
-        "parent": "https://forms.yandex.com/cloud/6797038e84227c1a123e374c/success/?page=1&akey=a697be9432e42266daef4a086b521e0ff70d7a98",
-        "dryRun": false
-    };
-    fetch("https://forms.yandex.com/cloud/gateway/root/form/postSurvey", {
-        "headers": {
-            "accept": "application/json, text/plain, */*",
-            "accept-language": "en",
-            "cache-control": "no-cache",
-            "content-type": "application/json",
-            "pragma": "no-cache",
-            // "priority": "u=1, i",
-            // "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-            // "sec-ch-ua-mobile": "?0",
-            // "sec-ch-ua-platform": "\"macOS\"",
-            // "sec-fetch-dest": "empty",
-            // "sec-fetch-mode": "cors",
-            // "sec-fetch-site": "same-origin",
-            "x-csrf-token": "cef648e1e98eb60e03015e1987ee62916e066584:1737951367",
-            "x-forms-yandexuid": "7579547131589821504",
-            "x-use-collab": "1"
-        },
-        "referrer": "https://forms.yandex.com/cloud/6797038e84227c1a123e374c/?page=1",
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": JSON.stringify(bodyJson),
-        "method": "POST",
-        // "mode": "cors",
-        "mode": "no-cors",
-        "credentials": "include"
-    }).then(response => {
-        console.log(response);
-    }).catch(error => {
-        console.error(error);
-    });
+    alert('The website is under development and this form does not work yet. In the meantime you can contact us thru X: https://x.com/NikitaYVolkov');
 });
 
 // Scroll animations
