36be3ae059c3610172a17c4ee453372ef848a624
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-01-29 19:29:12 +0300

    Update the scripts

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,7 @@
 // Form
-function renderBodyJson(name, company, email) {
-    return {
-        "surveyId": "6797038e84227c1a123e374c",
-        "values": { "name": name, "company": company, "email": email },
-        "parent": "https://forms.yandex.com/cloud/6797038e84227c1a123e374c/success/?page=1&akey=a697be9432e42266daef4a086b521e0ff70d7a98",
-        "dryRun": false
-    };
-}
-
 document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    alert('The website is under development and this form does not work yet. In the meantime you can contact us thru X: https://x.com/NikitaYVolkov');
+    alert('The website is under development and this form does not work yet. Sorry. In the meantime you can contact us thru X: https://x.com/NikitaYVolkov');
 });
 
 // Scroll animations
